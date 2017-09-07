600a2a39b0be6e05193c3cc14babbafd0ff4df29
vinson0616 <31696677+vinson0616@users.noreply.github.com>
2017-09-07 13:49:00 +0800

    Load the index.html successfully.

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -135,5 +135,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+STATICFILES_DIRS =[os.path.join(BASE_DIR,"static")]
