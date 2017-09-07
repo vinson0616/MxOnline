9d7c68c1c243d701933cb26c8a6802eb6878eb21
vinson0616 <31696677+vinson0616@users.noreply.github.com>
2017-09-07 12:13:43 +0800

    Initial commit

---
+++ apps/courses/adminx.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+__authoer__ = 'Vinson'
+__date__ = '9/6/2017 04:48 PM'
+
+import xadmin
+from .models import Course, Lesson, Video, CourseResource
+
+
+class CourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+
+
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    list_filter = ['course__name', 'name', 'add_time'] # course__name表示外键的名称
+
+
+class VideoAdmin(object):
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson', 'name', 'add_time']
+
+
+class CourseResourceAdmin(object):
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course', 'name', 'download', 'add_time']
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson,LessonAdmin)
+xadmin.site.register(Video,VideoAdmin)
+xadmin.site.register(CourseResource,CourseResourceAdmin)
---
+++ apps/organization/adminx.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+__authoer__ = 'Vinson'
+__date__ = '9/6/2017 04:59 PM'
+
+import xadmin
+from .models import CourseOrg, CityDict, Teacher
+
+
+class CityDictAdmin(object):
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums']
+    search_fields =['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums']
+
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org', 'name', 'work_years', 'work_company']
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg,CourseOrgAdmin)
+xadmin.site.register(Teacher,TeacherAdmin)
